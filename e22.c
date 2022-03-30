8e06f59cac9e2423b28b9776e3de3ff05f3fa675
rourou <rouroujun945@gmail.com>
2022-03-30 21:13:58 +0800

    Initial commit

---
+++ e22.c
@@ -0,0 +1,175 @@
+#include "bsp_e22.h"
+#include <string.h>
+#include <stdio.h>
+
+__STATIC_INLINE void Set_M0_High(void) {
+    HAL_GPIO_WritePin(E22_M0_GPIO_Port, E22_M0_Pin, GPIO_PIN_SET);
+}
+__STATIC_INLINE void Set_M0_Low(void) {
+    HAL_GPIO_WritePin(E22_M0_GPIO_Port, E22_M0_Pin, GPIO_PIN_RESET);
+}
+__STATIC_INLINE void Set_M1_High(void) {
+    HAL_GPIO_WritePin(E22_M1_GPIO_Port, E22_M1_Pin, GPIO_PIN_SET);
+}
+__STATIC_INLINE void Set_M1_Low(void) {
+    HAL_GPIO_WritePin(E22_M1_GPIO_Port, E22_M1_Pin, GPIO_PIN_RESET);
+}
+__STATIC_INLINE void Wait_For_E22(void) {
+    HAL_Delay(15);
+    while (HAL_GPIO_ReadPin(E22_AUX_GPIO_Port, E22_AUX_Pin) == GPIO_PIN_RESET) {}
+    HAL_Delay(5); //模块手册建议
+}
+typedef struct {
+    uint8_t  buffer[E22_UART_BUFFER_MAX_LENGTH];
+    uint16_t length;
+} e22_uart_packet_struct_t;
+
+typedef struct {
+    uint8_t                  count;
+    uint8_t                  inPos;
+    uint8_t                  outPos;
+    e22_uart_packet_struct_t packets[E22_UART_BUFFER_DEPTH];
+    uint8_t                  send_buffer[E22_UART_BUFFER_MAX_LENGTH];
+} e22_uart_buffer_struct_t;
+
+static e22_uart_buffer_struct_t e22_uart_buffer;
+
+void E22_FullSet_Register(E22_ConfigTypeDef *E22_ConfigStruct) {
+    E22_Mode(E22_Mode_Config);
+    // 从0字节开始传输9字节数据
+    e22_uart_buffer.send_buffer[0] = 0xC0;
+    e22_uart_buffer.send_buffer[1] = 0x00;
+    e22_uart_buffer.send_buffer[2] = 0x09;
+    e22_uart_buffer.send_buffer[3] = (0xFF00 & (E22_ConfigStruct->Address)) >> 8;
+    e22_uart_buffer.send_buffer[4] = (0x00FF & (E22_ConfigStruct->Address));
+    e22_uart_buffer.send_buffer[5] = E22_ConfigStruct->NetID;
+    e22_uart_buffer.send_buffer[6] =
+        E22_ConfigStruct->UARTSpeed | E22_ConfigStruct->ParityBit | E22_ConfigStruct->AirRate;
+    e22_uart_buffer.send_buffer[7] =
+        E22_ConfigStruct->SubPacket | E22_ConfigStruct->RSSI | E22_ConfigStruct->TransmissionPower;
+    e22_uart_buffer.send_buffer[8] = E22_ConfigStruct->Channel;
+    e22_uart_buffer.send_buffer[9] = E22_ConfigStruct->RSSIByte | E22_ConfigStruct->TransmissionMode
+                                     | E22_ConfigStruct->Repeater | E22_ConfigStruct->LBT
+                                     | E22_ConfigStruct->WORControl | E22_ConfigStruct->WORPeriod;
+    e22_uart_buffer.send_buffer[10] = (0xFF00 & (E22_ConfigStruct->CRYPT)) >> 8;
+    e22_uart_buffer.send_buffer[11] = (0x00FF & (E22_ConfigStruct->CRYPT));
+    Wait_For_E22();
+    HAL_UART_Transmit_DMA(&E22_UART_HANDLE, e22_uart_buffer.send_buffer, 12);
+}
+
+void E22_Read_PID(void) {
+    E22_Mode(E22_Mode_Config);
+    e22_uart_buffer.send_buffer[0] = 0xC1;
+    e22_uart_buffer.send_buffer[1] = 0x80;
+    e22_uart_buffer.send_buffer[2] = 0x07;
+    Wait_For_E22();
+    HAL_UART_Transmit_DMA(&E22_UART_HANDLE, e22_uart_buffer.send_buffer, 3);
+}
+void E22_Read_Register(void) {
+    E22_Mode(E22_Mode_Config);
+    e22_uart_buffer.send_buffer[0] = 0xC1;
+    e22_uart_buffer.send_buffer[1] = 0x00;
+    e22_uart_buffer.send_buffer[2] = 0x09;
+    Wait_For_E22();
+    HAL_UART_Transmit_DMA(&E22_UART_HANDLE, e22_uart_buffer.send_buffer, 3);
+}
+
+void E22_Mode(E22Mode_TypeDef mode) {
+    Wait_For_E22();
+
+    switch (mode) {
+    case E22_Mode_Transmission:
+        Set_M0_Low();
+        Set_M1_Low();
+        break;
+    case E22_Mode_WOR:
+        Set_M0_High();
+        Set_M1_Low();
+        break;
+    case E22_Mode_Config:
+        Set_M0_Low();
+        Set_M1_High();
+        break;
+    case E22_Mode_Sleep:
+        Set_M0_High();
+        Set_M1_High();
+        break;
+    default: break;
+    }
+    Wait_For_E22();
+}
+
+void E22_Buffer_Reset(void) {
+    //缓存初始化
+    e22_uart_buffer.count  = 0;
+    e22_uart_buffer.inPos  = 0;
+    e22_uart_buffer.outPos = 0;
+}
+
+uint8_t E22_UART_Packet_Pop(uint8_t *buffer) {
+    e22_uart_packet_struct_t *packet = &e22_uart_buffer.packets[e22_uart_buffer.outPos];
+    //无缓存时返回0
+    if (e22_uart_buffer.count == 0) { return 0; }
+    memcpy(buffer, packet->buffer, sizeof(uint8_t) * (packet->length));
+    (e22_uart_buffer.outPos)++;
+    if (e22_uart_buffer.outPos == E22_UART_BUFFER_DEPTH) { e22_uart_buffer.outPos = 0; }
+    // char tmp[30];
+    // sprintf(tmp, "count=%d,length=%d\n", e22_uart_buffer.count, packet->length);
+    // __DEBUG_UART_SendString(tmp);
+    (e22_uart_buffer.count)--;
+    return packet->length;
+}
+
+uint8_t E22_UART_Buffer_isEmpty(void) {
+    if (e22_uart_buffer.count == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void E22_Set(void) {
+    E22_ConfigTypeDef E22_ConfigStruct;
+    E22_ConfigStruct.Address           = (uint16_t)0x0001;
+    E22_ConfigStruct.NetID             = (uint8_t)0x01;
+    E22_ConfigStruct.UARTSpeed         = E22_UARTSpeed_9600;
+    E22_ConfigStruct.ParityBit         = E22_UARTParityBit_8N1;
+    E22_ConfigStruct.AirRate           = E22_AirRate_2400;
+    E22_ConfigStruct.SubPacket         = E22_SubPacket_240B;
+    E22_ConfigStruct.RSSI              = E22_RSSI_Disable;
+    E22_ConfigStruct.TransmissionPower = E22_TransmissionPower_22dBm;
+    E22_ConfigStruct.Channel           = (uint8_t)23;
+    E22_ConfigStruct.RSSIByte          = E22_RSSIByte_Disable;
+    E22_ConfigStruct.TransmissionMode  = E22_TransmissionMode_FixedPoint;
+    E22_ConfigStruct.Repeater          = E22_Repeater_Disable;
+    E22_ConfigStruct.LBT               = E22_LBT_Disable;
+    E22_ConfigStruct.WORControl        = E22_WOR_Receiver;
+    E22_ConfigStruct.WORPeriod         = E22_WORPeriod_1000ms;
+    E22_ConfigStruct.CRYPT             = (uint16_t)0x1234;
+    E22_FullSet_Register(&E22_ConfigStruct);
+}
+
+void E22_StartReceive(void) {
+    e22_uart_packet_struct_t *currentPacket = &e22_uart_buffer.packets[e22_uart_buffer.inPos];
+    HAL_UARTEx_ReceiveToIdle_DMA(&E22_UART_HANDLE, currentPacket->buffer,
+                                 E22_UART_BUFFER_MAX_LENGTH);
+}
+
+void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size) {
+    if (huart->Instance == E22_UART_HANDLE.Instance) {
+        e22_uart_packet_struct_t *currentPacket = &e22_uart_buffer.packets[e22_uart_buffer.inPos];
+        currentPacket->length                   = Size;
+        e22_uart_buffer.inPos++;
+        if (e22_uart_buffer.inPos == E22_UART_BUFFER_DEPTH) { e22_uart_buffer.inPos = 0; }
+        e22_uart_buffer.count++;
+        E22_StartReceive();
+    }
+}
+void E22_Send(uint16_t address, uint8_t channel, uint8_t *data, uint16_t length) {
+    e22_uart_buffer.send_buffer[0] = (address & 0xFF00) >> 8;
+    e22_uart_buffer.send_buffer[1] = (address & 0x00FF);
+    e22_uart_buffer.send_buffer[2] = channel;
+    memcpy(&e22_uart_buffer.send_buffer[3], data, sizeof(uint8_t) * length);
+    Wait_For_E22();
+    HAL_UART_Transmit_DMA(&E22_UART_HANDLE, e22_uart_buffer.send_buffer, length + 3);
+}
---
+++ e22.h
@@ -0,0 +1,165 @@
+#ifndef __E22_H
+#define __E22_H
+#include "main.h"
+#include "usart.h"
+
+#define E22_UART_BUFFER_MAX_LENGTH (240)
+#define E22_UART_BUFFER_DEPTH      (20)
+
+#define E22_UART_HANDLE            huart2
+
+typedef enum {
+    E22_Mode_Transmission = 0x00, //!< E22传输模式
+    E22_Mode_WOR          = 0x01, //!< E22WOR模式
+    E22_Mode_Config       = 0x02, //!< E22配置模式
+    E22_Mode_Sleep        = 0x03  //!< E22睡眠模式
+} E22Mode_TypeDef;
+// REG0
+typedef enum {
+    E22_UARTSpeed_1200   = 0x00,
+    E22_UARTSpeed_2400   = 0x20,
+    E22_UARTSpeed_4800   = 0x40,
+    E22_UARTSpeed_9600   = 0x60,
+    E22_UARTSpeed_19200  = 0x80,
+    E22_UARTSpeed_38400  = 0xA0,
+    E22_UARTSpeed_57600  = 0xC0,
+    E22_UARTSpeed_115200 = 0xE0,
+} E22UARTSpeed_TypeDef;
+
+typedef enum {
+    E22_UARTParityBit_8N1 = 0x00,
+    E22_UARTParityBit_8O1 = 0x08,
+    E22_UARTParityBit_8E1 = 0x10,
+} E22UARTParityBit_TypeDef;
+
+typedef enum {
+    E22_AirRate_300   = 0x00,
+    E22_AirRate_1200  = 0x01,
+    E22_AirRate_2400  = 0x02,
+    E22_AirRate_4800  = 0x03,
+    E22_AirRate_9600  = 0x04,
+    E22_AirRate_19200 = 0x05,
+    E22_AirRate_38400 = 0x06,
+    E22_AirRate_62500 = 0x07,
+} E22AirRate_TypeDef;
+// REG1
+typedef enum {
+    E22_SubPacket_240B = 0x00,
+    E22_SubPacket_128B = 0x40,
+    E22_SubPacket_64B  = 0x80,
+    E22_SubPacket_32B  = 0xC0,
+} E22SubPacket_TypeDef;
+
+typedef enum {
+    E22_RSSI_Disable = 0x00,
+    E22_RSSI_Enable  = 0x20,
+} E22RSSI_TypeDef;
+
+typedef enum {
+    E22_TransmissionPower_22dBm = 0x00,
+    E22_TransmissionPower_17dBm = 0x01,
+    E22_TransmissionPower_13dBm = 0x02,
+    E22_TransmissionPower_10dBm = 0x03,
+} E22TransmissionPower_TypeDef;
+// REG3
+typedef enum {
+    E22_RSSIByte_Disable = 0x00,
+    E22_RSSIByte_Enable  = 0x80,
+} E22RSSIByte_TypeDef;
+
+typedef enum {
+    E22_TransmissionMode_Transparent = 0x00,
+    E22_TransmissionMode_FixedPoint  = 0x40,
+} E22TransmissionMode_TypeDef;
+
+typedef enum {
+    E22_Repeater_Disable = 0x00,
+    E22_Repeater_Enable  = 0x20,
+} E22Repeater_TypeDef;
+
+typedef enum {
+    E22_LBT_Disable = 0x00,
+    E22_LBT_Enable  = 0x10,
+} E22LBT_TypeDef;
+
+typedef enum {
+    E22_WOR_Receiver    = 0x00,
+    E22_WOR_Transmitter = 0x08,
+} E22WORControl_TypeDef;
+
+typedef enum {
+    E22_WORPeriod_500ms  = 0x00,
+    E22_WORPeriod_1000ms = 0x01,
+    E22_WORPeriod_1500ms = 0x02,
+    E22_WORPeriod_2000ms = 0x03,
+    E22_WORPeriod_2500ms = 0x04,
+    E22_WORPeriod_3000ms = 0x05,
+    E22_WORPeriod_3500ms = 0x06,
+    E22_WORPeriod_4000ms = 0x07,
+} E22WORPeriod_TypeDef;
+
+typedef struct {
+    // 00H~01H
+    uint16_t                     Address;
+    // 02H
+    uint8_t                      NetID;
+    // 03H
+    E22UARTSpeed_TypeDef         UARTSpeed;
+    E22UARTParityBit_TypeDef     ParityBit;
+    E22AirRate_TypeDef           AirRate;
+    // 04H
+    E22SubPacket_TypeDef         SubPacket;
+    E22RSSI_TypeDef              RSSI;
+    E22TransmissionPower_TypeDef TransmissionPower;
+    // 05H
+    uint8_t                      Channel;
+    // 06H
+    E22RSSIByte_TypeDef          RSSIByte;
+    E22TransmissionMode_TypeDef  TransmissionMode;
+    E22Repeater_TypeDef          Repeater;
+    E22LBT_TypeDef               LBT;
+    E22WORControl_TypeDef        WORControl;
+    E22WORPeriod_TypeDef         WORPeriod;
+    // 07H~08H
+    uint16_t                     CRYPT;
+} E22_ConfigTypeDef;
+
+void E22_Buffer_Reset(void);
+void E22_Read_PID(void);
+void E22_Read_Register(void);
+void E22_FullSet_Register(E22_ConfigTypeDef *E22_ConfigStruct);
+void E22_Mode(E22Mode_TypeDef mode);
+
+/**
+ * @brief 从E22接收缓存队列中取出一个包
+ *
+ * @param buffer 缓存包数据
+ * @retval uint8_t 缓存包长度
+ */
+uint8_t E22_UART_Packet_Pop(uint8_t *buffer);
+/**
+ * @brief 检查E22接收缓存队列中是否还有未取出的包
+ *
+ * @retval uint8_t T/F
+ */
+uint8_t E22_UART_Buffer_isEmpty(void);
+/**
+ * @brief 配置E22
+ *
+ */
+void    E22_Set(void);
+/**
+ * @brief 开启E22串口接收中断
+ *
+ */
+void    E22_StartReceive(void);
+/**
+ * @brief
+ *
+ * @param address  目标地址
+ * @param channel  目标信道
+ * @param data     数据首地址
+ * @param length   数据包长度
+ */
+void    E22_Send(uint16_t address, uint8_t channel, uint8_t *data, uint16_t length);
+#endif // !__E22_H
